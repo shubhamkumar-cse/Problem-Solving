218c2cb30609cc3d21d9ab635e88b46842856328
shubhamkuamr-cse <shubham4003singh@gmail.com>
2026-02-23 21:18:04 +0530

    Task Done

---
+++ Strings/Uppercase.java
@@ -0,0 +1,6 @@
+public class Uppercase {
+    public static void main(String[] args) {
+        String str="Suraj Singh Rathore";
+        System.out.println(str.toUpperCase());
+    }
+}
---
+++ Strings/lowercase.java
@@ -0,0 +1,6 @@
+public class lowercase {
+    public static void main(String[] args) {
+        String str="SURAJ Singh Rathore";
+        System.out.println(str.toLowerCase());
+    }
+}
